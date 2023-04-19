230be74b19e8868be5d808490aed0cd9718b0d94
yladev <yanlinnaung881@gmail.com>
2023-04-19 22:07:12 +0630

    update sidebar notification and messages javascript

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// SideBar
+const menuItems = document.querySelectorAll('.menu-item');
+
+// remove active class from all menu items
+const changeActiveItem = () => {
+   menuItems.forEach(item => {
+    item.classList.remove('active');
+   }) 
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id !== 'notifications') {
+            document.querySelector('.notifications-popup').style.display = 'none';
+        } else {
+            document.querySelector('.notifications-popup').style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
+        }
+    })
+})
+
+// Messages
+const messagesNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
