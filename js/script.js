272caf38abdf5d900385eb24b25a122287defba2
yladev <yanlinnaung881@gmail.com>
2023-04-20 18:06:04 +0630

    update filter messages javascript

--- js/script.js
+++ js/script.js
@@ -24,7 +24,10 @@ menuItems.forEach(item => {
 // Messages
 const messagesNotification = document.querySelector('#messages-notifications');
 const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
 
+// hightlight messages card when messages menu item is clicked
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
     messagesNotification.querySelector('.notification-count').style.display = 'none';
@@ -32,3 +35,19 @@ messagesNotification.addEventListener('click', () => {
         messages.style.boxShadow = 'none';
     }, 2000);
 })
+
+// searches chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1) {
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none';
+        }
+    })
+}
+
+// search chat
+messageSearch.addEventListener('keyup', searchMessage);
